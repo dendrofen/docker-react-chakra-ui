04f30bca8984bf08edcd8c8843fb499cf45c3c65
dendrofen <ma9k97dev@gmail.com>
2023-02-03 23:59:12 +0200

    feat: remove unused files, upgrade packages

--- src/theme.js
+++
@@ -1,9 +0,0 @@
-import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react'
-
-const theme = extendTheme({
-    config: {
-        initialColorMode: 'light',
-        useSystemColorMode: true,
-    }
-}, withDefaultColorScheme({ colorScheme: 'telegram' }));
-export default theme;
